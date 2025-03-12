f57eef8395fbb6c0f9df3376ffef3f1b2da58713
tofmat <tofmatt@gmail.com>
2025-03-12 07:51:33 +0100

    feat: authenication ui

--- app/(home)/ambassador-dao/page.tsx
+++ app/(home)/ambassador-dao/page.tsx
@@ -1,12 +1,17 @@
-import { redirect } from 'next/navigation'
+"use client";
+import { SignupModal } from "@/components/ambassador-dao/sections/signup-modal";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 const AmbasssadorDao = () => {
+  const [singupModal, setSignupModal] = useState(false);
   return (
-    <div>Gigs Page
+    <div>
+      <Button onClick={() => setSignupModal(true)}>Onboard</Button>
 
-    
+      <SignupModal isOpen={singupModal} onClose={() => setSignupModal(false)} />
     </div>
-  )
-}
+  );
+};
 
-export default AmbasssadorDao
+export default AmbasssadorDao;
